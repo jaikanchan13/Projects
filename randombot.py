13b92f2c0c1aaa793697eea47e8d45a789e5263f
jaikanchan13 <147464669+jaikanchan13@users.noreply.github.com>
2026-06-09 18:43:52 -0400

    Delete randombot.py

--- randombot.py
+++
@@ -1,9 +0,0 @@
-import random
-responses= ["hi", "hello", "how are you"]
-
-while True:
-    question = input("You: ").lower()
-    if question == "bye":
-        print("Bot: GoodBye")
-        break
-    print ("Bot: "+random.choice(responses))
